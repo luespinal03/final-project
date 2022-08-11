bc5787ef3c0909f2eaa590dbcb7cb969421a06c6
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-08-11 15:05:51 -0400

    final-project

--- main.js
+++ main.js
@@ -1,27 +1,43 @@
 //landing page queries
 const signUpSubmit = document.querySelector('#signUpSubmit')
-const signUpEmail = document.querySelector('#signUpEmail')
+const signUpUserName = document.querySelector('#signUpUserName')
 const signUpPassword = document.querySelector('#signUpPassword')
 
 //login page queries
 const loginSubmit = document.querySelector('#loginSubmit')
-const loginEmail = document.querySelector('#loginEmail')
+const loginUserName = document.querySelector('#loginUserName')
 const loginPassword = document.querySelector('#loginPassword')
 
 //initial setup page
 const takeMeButton = document.querySelector('#takeMetoMyPage')
 
 //custom page/////
-let customProfileCardLikes = document.querySelector("customProfileCardLikes")
+let customProfileCardLikes = document.querySelector("#customProfileCardLikes")
 const dropDownItems = document.querySelectorAll('#sideNav a')
+
+let postButton = document.querySelector('#postButton');
+let status = document.querySelector('#status');
+let statusPost = document.querySelector('#statusPost');
+let cardName = document.querySelector('.card-title')
+
+// This controls the status post button and where it displays (right undeneath it)
+try {
+    postButton.addEventListener('click', () => {
+        statusPost.innerText = status.value;
+        status.value = ''
+    });
+} catch (error) {
+    console.log(error);
+}
+
 
 //////users//////////
 let user1 = {
-    email: 'luisespinal296@yahoo.com',
+    userName: 'luisespinal296@yahoo.com',
     password: 'thisbetterwork'
 }
 let user2 = {
-    email: 'claytonses@yahoo.com',
+    userName: 'claytonses@yahoo.com',
     password: 'test123'
 }
 let user = {}
@@ -36,33 +52,47 @@ console.log(signUpSubmit)
 
 try {
     signUpSubmit.addEventListener('click', () => {
-        if (signUpEmail.value !== '' && signUpPassword.value !== '') {
-            user.email = signUpEmail.value;
+        localStorage.clear();
+        if (signUpUserName.value !== '' && signUpPassword.value !== '') {
+            user.userName = signUpUserName.value;
             user.password = signUpPassword.value;
             console.log(user)
+            localStorage.setItem('user', JSON.stringify(user));
             window.location.href = './initSetup.html'
         } else {
             alert('Please Fill out the Empty Fields')
         }
     });
 } catch (error) {
-    console.log(error)
+    console.log(error);
 }
-
+try {
+    let localLiked = localStorage.getItem('liked');
+    let localUser = localStorage.getItem('user');
+    if (localLiked !== null) {
+        user = JSON.parse(localUser)
+        likeItems = JSON.parse(localLiked);
+        customProfileCardLikes.innerText = `Liked: ${likeItems}`
+        cardName.innerText = user.userName;
+    }
+} catch (error) {
+    console.log(error);
+}
 
 
 try {
     takeMeButton.addEventListener('click', () => {
+        console.log('hi')
         for (let i = 0; i < likes.length; i++) {
             if (likes[i].checked === true) {
-                likeItems.push(likes[i].id)
+                likeItems.push(likes[i].value)
                 console.log(likeItems)
-                window.location.href = './customProfile.html'
-
-            } else {
-                window.location.href = './customProfile.html'
             }
         }
+        localStorage.setItem('liked', JSON.stringify(likeItems));
+        window.location.href = './customProfile.html'
+
+
     });
 } catch (error) {
     console.log(error);
@@ -73,7 +103,7 @@ console.log(likeItems)
 try {
     loginSubmit.addEventListener('click', () => {
         console.log('yes')
-        if ((loginEmail.value === user1.email && loginPassword.value === user1.password) || (loginEmail.value === user2.email && loginPassword.value === user2.password)) {
+        if ((loginUserName.value === user1.userName && loginPassword.value === user1.password) || (loginUserName.value === user2.userName && loginPassword.value === user2.password)) {
             console.log('we do it?')
             window.location.href = './customProfile.html'
         } else {
